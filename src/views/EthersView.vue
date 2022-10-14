c878d6c823f5765323b9c37f8241892f1ce84fe9
Tom Medhurst <tom.medhurst@gmail.com>
2022-10-14 19:49:31 +0100

    call a contract from within an app

--- src/views/EthersView.vue
+++ src/views/EthersView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="about">
     <h1>ethers.js test page</h1>
+    <div v-if="error">ERROR: {{ error }}</div>
     <p>
       Your balance is: <code>{{ balance }}</code><br/>
       From address: <code>{{ address }}</code><br/>
       From network: <code>{{ network }}</code><br/>
+      Signing <code>{{ messageToSign }}</code> as <code>{{ signedMessage }}</code><br/>
+      Message from Contract: <code>{{ messageFromContract }}</code><br/>
     </p>
     <div class="buttons">
       <button @click="connect">Connect</button>
+      <button @click="sign">Sign</button>
+      <button @click="callContract">Call Contract</button>
     </div>
   </div>
 </template>
@@ -15,13 +20,18 @@
 <script setup>
 import { ref } from 'vue'
 import { ethers } from 'ethers'
+import CLHelloWorld from '@/contracts/CLHelloWorld.json'
+import CLHelloWorldAddr from '@/contracts/CLHelloWorldAddr.json'
 
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-let signer = null // provider.getSigner()
+const provider = new ethers.providers.Web3Provider(window.ethereum, 'goerli')
+let signer = null
 const balance = ref(0)
 const address = ref(0)
 const network = ref('unknown')
 const error = ref(null)
+const messageToSign = "hello world"
+const signedMessage = ref(null)
+const messageFromContract = ref(null)
 
 async function connect() {
   try {
@@ -33,5 +43,20 @@ async function connect() {
   } catch(e) {
     error.value = e.message
   }
+}
+
+async function sign () {
+  // sign a hash?
+  //const messageHash = ethers.utils.id(messageToSign)
+  //const messageHashBytes = ethers.utils.arrayify(messageHash)
+  // sign actual string (so users can see what they're signing in MetaMask)
+  const messageHashBytes = ethers.utils.toUtf8Bytes(messageToSign)
+  signedMessage.value = await signer.signMessage(messageHashBytes)
+}
+
+async function callContract () {
+  const addr = CLHelloWorldAddr.address
+  const contract = new ethers.Contract(addr, CLHelloWorld.abi, signer)
+  messageFromContract.value = await contract.message();
 }
 </script>
